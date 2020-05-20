da3e6cf895ad3335f15e00a61de779056123421f
Garnet3106 <manage@gant.work>
2020-05-21 00:08:46 +0900

    Add EEWData class and EEW data analyzer

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const fs = require('fs');
 const http = require('http');
+const request = require('request');
+const client = new Discord.Client();
 const token = process.env.DISCORD_BOT_TOKEN;
 const prefix = 'eew.';
 
@@ -148,7 +149,6 @@ var alarmChannels = [];
 function loadAlarmChannels() {
     let data = fs.readFileSync('alarm_channels.txt', 'utf8');
     alarmChannels = data != '' ? data.split(',') : []
-    console.log(alarmChannels);
 }
 
 
@@ -156,6 +156,124 @@ function recordAlarmChannels() {
     console.log(alarmChannels.join(','));
     fs.writeFileSync('alarm_channels.txt', alarmChannels.join(','), 'utf8');
 }
+
+
+/* EEW受信 */
+
+
+var eewData = null;
+var latestEventID = null;
+
+
+class EEWData {
+    constructor() {
+        // 速報の種類 | -1: 不明, 0: 予報, 1: 警報, 2: キャンセル報
+        this.type = -1;
+
+        // ソース名
+        this.source = '';
+
+        // 発生時刻 (Unixタイムスタンプ)
+        this.timestamp = 0;
+
+        // 発表時刻 (Unixタイムスタンプ)
+        this.announceTimestamp = 0;
+
+        // 震源地
+        this.hypocenter = '';
+
+        // 最大震度
+        this.maxIntensity = '';
+
+        // マグニチュード
+        this.magnitude = 0;
+
+        // 深さ
+        this.depth = 0;
+
+        // 緯度
+        this.latitude = 0;
+
+        // 経度
+        this.longitude = 0;
+
+        // 警報の対象地域 (都道府県)
+        this.warnPrefectures = [];
+
+        // 警報の対象地域 (気象庁の震央地名)
+        this.warnJMAAreas = [];
+    }
+}
+
+
+function analyzeEEWData(json) {
+    try {
+        eewData = new EEWData();
+
+        // 速報の種類
+        eewData.type = 'Hypocenter' in json ? (!json['Warn'] ? 0 : 1) : 2;
+
+        // ソース名
+        eewData.source = json['Source']['String'];
+
+        // 発生時刻 (Unixタイムスタンプ)
+        eewData.timestamp = json['OriginTime']['UnixTime'];
+
+        // 発表時刻 (Unixタイムスタンプ)
+        eewData.announceTimestamp = json['AnnouncedTime']['UnixTime'];
+
+        if(eewData.type != 2) {
+            // 震源地
+            eewData.hypocenter = json['Hypocenter']['Name'];
+
+            // 最大震度
+            eewData.maxIntensity = json['MaxIntensity']['To'];
+
+            // マグニチュード
+            eewData.magnitude = json['Hypocenter']['Magnitude']['Float'];
+
+            // 深さ
+            eewData.depth = json['Hypocenter']['Location']['Depth']['Int'];
+
+            // 緯度
+            eewData.latitude = json['Hypocenter']['Location']['Lat'];
+
+            // 経度
+            eewData.longitude = json['Hypocenter']['Location']['Long'];
+        }
+
+        if(eewData.type == 1) {
+            // 警報の対象地域 (都道府県)
+            eewData.warnPrefectures = json['WarnForecast']['LocalAreas'];
+
+            // 警報の対象地域 (気象庁の震央地名)
+            eewData.warnJMAAreas = json['WarnForecast']['Regions'];
+        }
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+
+setInterval(() => {
+    request('https://api.iedred7584.com/eew/json', (err, res, body) => {
+        if(err) {
+            console.log('APIの接続に失敗しました。');
+            return;
+        }
+
+        let json = JSON.parse(body);
+
+        if(latestEventID != json['EventID']) {
+            if(latestEventID !== null) {
+                // 緊急地震速報を送信
+                let data = analyzeEEWData(json);
+            }
+
+            latestEventID = json['EventID'];
+        }
+    });
+}, 5000);
 
 
 /* 終了処理 */
